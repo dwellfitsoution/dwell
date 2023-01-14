e3d4a778c4aa1c24de397417f36e0adedb0a21a7
Info <dwellfitsoution@gmail.com>
2023-01-13 21:10:40 -0800

    Init commit

--- src/main/java/com/dwell/controller/CheckupController.java
+++ src/main/java/com/dwell/controller/CheckupController.java
@@ -1,9 +1,11 @@
 package com.dwell.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("check-up")
+@RestController
+@RequestMapping("/check-up")
 public class CheckupController {
     @GetMapping
     public String get(){
